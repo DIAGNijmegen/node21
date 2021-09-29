62620101985c2539badda0ba87a0bee055a26fbb
ecemlago <ecem.lago@radboudumc.nl>
2021-09-29 13:02:21 +0200

    added gitattributes and fixed errors in evaluation detection docker

--- evaluation/evaluation_detection/test.sh
+++ evaluation/evaluation_detection/test.sh
@@ -4,16 +4,16 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 ./build.sh
 
-docker volume create evaluation_detection-output
+docker volume create eval-output
 
 docker run --rm \
         --memory=4g \
         -v $SCRIPTPATH/test/:/input/ \
-        -v evaluation_detection-output:/output/ \
+        -v eval-output:/output/ \
         evaluation_detection
 
 docker run --rm \
-        -v evaluation_detection-output:/output/ \
+        -v eval-output:/output/ \
         python:3.7-slim cat /output/metrics.json | python -m json.tool
 
-docker volume rm evaluation_detection-output
+docker volume rm eval-output
